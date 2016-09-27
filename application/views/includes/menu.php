2d83fafc1b9756a416706443b569b468cc3d9032
Pedro Henrique <pedro.brito@fortaleza.ce.gov.br>
2016-09-27 15:06:51 -0300

    Removido a condição para ocultar o menu

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -11,26 +11,23 @@
 	      <a class="navbar-brand" href="#">Ok Design</a>
 	    </div>
 
-		<?php if(@!$hidemenu){ ?>
-
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        
+
 	        <li class="dropdown">
 	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Placas <span class="caret"></span></a>
 	          <ul class="dropdown-menu">
 	            <li><a href="<?php echo base_url('placa/visualizar_todos'); ?>">Visualizar</a></li>
 	            <li><a href="<?php echo base_url('placa/cadastrar'); ?>">Cadastrar</a></li>
-	            
+
 	          </ul>
 	        </li>
-	        
+
 	      </ul>
-	      
-	      
+
+
 	    </div><!-- /.navbar-collapse -->
-	    <?php } ?>
 
 	  </div><!-- /.container-fluid -->
 	</nav>
